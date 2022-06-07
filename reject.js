1ddc27840def13aab7e7b72d8730c993295d345b
wmshoon <97453378+wmshoon@users.noreply.github.com>
2022-06-07 19:15:34 +0900

    코드 최적화+ 사이트 추가

--- reject.js
+++ reject.js
@@ -1,11 +1,19 @@
 var agent = navigator.userAgent.toLowerCase();
 if (!agent.indexOf("w3m/0.5.3+git20200502") != -1) {
-    window.location.href = `${randomWebsite()}`;
+    window.location.href = `${kickToRandomSite()}`;
 }
 
-function randomWebsite() {
-    const list = ["http://www.google.com", "http://www.yahoo.com", "https://www.bing.com", "https://www.yahoo.com  "];
+function kickToRandomSite() {
+    const list = [
+        "http://www.google.com",
+        "http://www.yahoo.com",
+        "https://www.bing.com",
+        "https://www.yahoo.com",
+        "https://www.daum.net",
+        "https://www.naver.com",
+        "https://duckduckgo.com/",
+    ];
     let a = Math.random();
-    a = parseInt(a * 4);
+    a = parseInt(a * list.lenth);
     return list[a];
 }
